e95690cca10e73e041791e2212eb4a5e68e56064
Alberto Barrilà <alberto.barrila@gmail.com>
2021-01-29 10:16:25 +0100

    lesson 13 - matching heads and tails

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -36,6 +36,10 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{conv | status: 201,  resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!"}
+  end
+
   def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
     Path.expand("../../pages", __DIR__)
       |> Path.join("form.html")
@@ -169,3 +173,18 @@ Accept: */*
 """
 
 IO.puts(Servy.Handler.handle(request))
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
