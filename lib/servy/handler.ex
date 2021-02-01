0062bfb7569e1b322fa0a670efd053427d032ac5
Alberto Barrilà <alberto.barrila@gmail.com>
2021-02-01 09:22:20 +0100

    lesson 15 - slicing and dicing with enums

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -5,6 +5,7 @@ defmodule Servy.Handler do
   """
 
   alias Servy.Conv
+  alias Servy.BearController
 
   @pages_path Path.expand("../../pages", __DIR__)
 
@@ -33,11 +34,11 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/bears"} = conv) do
-    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+    BearController.index(conv)
   end
 
   def route(%Conv{method: "POST", path: "/bears"} = conv) do
-    %{conv | status: 201,  resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!"}
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
@@ -48,7 +49,8 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
-    %{conv | status: 200, resp_body: "Bear #{id}"}
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   def route(%Conv{method: "GET", path: "/about"} = conv) do
@@ -66,7 +68,7 @@ defmodule Servy.Handler do
 end
 
   def route(%Conv{method: "DELETE", path: "/bears/" <> _id} = conv) do
-    %{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
+    BearController.delete(conv, conv.params)
   end
 
   def route(%Conv{path: path} = conv) do
